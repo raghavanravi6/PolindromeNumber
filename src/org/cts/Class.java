6d0c5260e47846ff417c8f7cb237b40980aefd7e
raghavanravi6 <raghavanravi662k@gmail.com>
2022-03-08 20:01:44 +0530

    Modify Class

--- src/org/cts/Class.java
+++ src/org/cts/Class.java
@@ -1,5 +1,16 @@
 package org.cts;
 
 public class Class {
+	public void empId() {
+		System.out.println("Employee id is : 2424");
+	}
+	public void empName() {
+		System.out.println("Employee name is Raghavan R");
+
+	}
+	public static void main(String[] args) {
+		Class c=new Class();
+		c.empId();c.empName();
+	}
 
 }
